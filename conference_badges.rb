b1c2550e0f9576220945ddf43613299dda3a14a0
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 20:46:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,9 @@
 
 #badge_maker("Ariel")
 
-def batch_badge_creator[]
-  batch_badge_creator[Edsger,Ada,Charles,Alan,Grace,Linus,Matz]
-  puts batch_badge_creator[names]
+def batch_badge_creator[name]
+  name[Edsger,Ada,Charles,Alan,Grace,Linus,Matz]
+  puts batch_badge_creator + name
 end
 
 batch_badge_creator[1]
