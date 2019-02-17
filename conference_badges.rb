d470fa1dda54dbd99da4918b492e5f568b9b31cf
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 21:00:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,21 @@
-#def badge_maker(name)
-#  puts ("hello, my name is " + name)
-#end
+def badge_maker(name)
+puts ("hello, my name is " + name)
+end
 
-#badge_maker("Ariel")
+badge_maker("Ariel")
 
+ 
+ 
+ 
+ 
   Array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-  puts  Array[1]
+  puts Array[1]
 
 
-#def assign_room[]
+
+
+
+
+def assign_room[]
   return ("Hello, " +"!" + " You'll be assigned to room " + "!")
-#end
+end
